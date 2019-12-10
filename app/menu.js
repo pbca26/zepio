435596b86d095ca5556e0d657fadd93e5d165985
pbca26 <pbca26@gmail.com>
2019-12-10 17:30:27 +0300

    app menu add reload, dev tools options

--- app/menu.js
+++ app/menu.js
@@ -23,6 +23,22 @@ const menu = [
   {
     label: 'View',
     submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click (item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.reload();
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click (item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.webContents.toggleDevTools();
+        }
+      },
       { role: 'togglefullscreen' },
     ],
   },
