2bf0eaf6ed1710565204cf6c9154c182ec91484e
pbca26 <pbca26@gmail.com>
2019-12-11 18:51:43 +0300

    change explorer link ZEC -> RICK

--- app/constants/explorer.js
+++ app/constants/explorer.js
@@ -4,4 +4,4 @@ import { isTestnet } from '../../config/is-testnet';
 
 export const ZCASH_EXPLORER_BASE_URL = isTestnet()
   ? 'https://chain.so/tx/ZECTEST/'
-  : 'https://zcha.in/transactions/';
+  : 'https://rick.kmd.dev/tx/';
